38fd9fe30be3aa0e3db980812e5ffdbb6dd468e8
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2024-05-29 16:24:29 -0300

    display all posts + buttons for actions

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 const urlBase = "https://jsonplaceholder.typicode.com/posts";
-let post = [];
+let posts = [];
 
 function getData() {
   fetch(urlBase)
     .then((res) => res.json())
     .then((data) => {
-      post = data;
-      console.log(data); /// metodo para mostrar la inf en pantalla
+      posts = data;
+      renderPostsList();
     })
     .catch((error) => console.log("Error al llamar a la api.", error));
+}
+
+function renderPostsList() {
+  const postsList = document.getElementById("postsList");
+  postsList.innerHTML = "";
+
+  posts.forEach((post) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("postItem");
+    listItem.innerHTML = `
+    <strong>${post.title}</strong>
+    <p>${post.body}</p>
+    <button onclick="editPost(${post.id})">Editar</button>
+    <button onclick="deletePost(${post.id})">Borrar</button>
+
+    <div id="editForm-${post.id}" class="editForm" >
+      <label for="editTitle">Título:</label>
+      <input id="editTitle-${post.id}" value="${post.title}" required>
+      <label for="editBody">Comentario:</label>
+      <textarea id="editBody-${post.id}" required></textarea>
+      <button onclick="updatePost(${post.id})">Actualizar</button>
+    </div>
+    `;
+    postsList.appendChild(listItem);
+  });
 }
